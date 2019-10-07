dfb425ef37866e82c31effeb47d3395132915c45
chrisanatalio <anataliocs@gmail.com>
2019-10-06 18:04:44 -0700

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -43,13 +43,12 @@ export default class Header extends React.Component {
           <div className="container h-100">
             <div className="row h-100 align-items-center justify-content-center text-center">
               <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
-                  Themes</h1>
+                <h1 className="text-white font-weight-bold">Project Mmmk Gotcha</h1>
                 <hr className="divider my-4"/>
               </div>
               <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
-                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
+                <p className="text-white-75 font-weight-light mb-5">Spooky, silly podcast that focus on the
+                  Macabre</p>
                 <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Find Out More</a>
               </div>
             </div>
